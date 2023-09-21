61e37193f74139fee6d00991fa64344b91198d95
satyam-seth <satyam1998.1998@gmail.com>
2023-09-22 01:07:40 +0530

    move counter logic in counter component

--- Geekyshows/3-react-js-counter-example-without-redux/react-redux-course/src/App.js
+++ Geekyshows/3-react-js-counter-example-without-redux/react-redux-course/src/App.js
@@ -1,15 +1,10 @@
 import './App.css';
-import { useState } from "react";
+import Counter from './features/counter/counter';
 
 function App() {
-  const [count, setCount] = useState(0);
   return (
     <div className="App">
-      <button className="button" aria-label="Increment value" onClick={() => { setCount(count + 1) }}> + </button>
-
-      <span className="value">Count: {count}</span>
-
-      <button className="button" aria-label="Decrement value" onClick={() => { setCount(count - 1) }}> - </button>
+      <Counter />
     </div>
   );
 }
